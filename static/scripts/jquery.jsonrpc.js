623382e36d3cf56d54785ff882a7895bc079d46a
D. Dotsenko <dotsa@hotmail.com>
2010-10-17 12:18:41 -0700

    Replaced 3rd party jQuery JSONRPC plug in with my own.
    Had some problems with the original plug in. Looked inside and could not figure out what is what. On top of the fact that it was written for JSONPRC v.2.0, and our server RPC does v.1.0, you had convoluted object instantiation all over the place. I am guessing the author is either a Ruby or Java enthusiast. Instead of spending time to figure out how to fix it, just looked at the spec and wrote my own. 40 lines of code. 50 lines of comments.

--- static/scripts/jquery.jsonrpc.js
+++ static/scripts/jquery.jsonrpc.js
@@ -1,98 +1,95 @@
-/**
- * Jsonrpc plugin
- * Intended for use with jQuery 1.4.2
- *
- * Copyright (c) 2010 Cloud4pc (www.cloud4pc.com)
- * Dual licensed under the MIT and GPL licenses:
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- *
- * @version 0.5.0
- *
+/*
+JSONRPC (protocol v1.0) plugin for jQuery
 
-Example 1
-$.JsonRPC.request ("http://xxx.xxx.xxx/jsonrpcServer", method, params, {
-        success: function (response) {
-                // {"id":1,"result":"...","error":null}
-        },
-        error: function (response) {
-                // {"id":1,"result":null,"error":"..."}
-        }
-});
-EXAMPLE 2
+Copyright (c) 2010  Daniel Dotsenko <dotsa (a) hotmail com>
 
-$.JsonRPC.endPoint = "http://xxx.xxx.xxx/jsonrpcServer";
-$.JsonRPC.request (null, method, params, {
-        success: function (response) {
-                // {"id":1,"result":"...","error":null}
-        },
-        error: function (response) {
-                // {"id":1,"result":null,"error":"..."}
-        }
-});
- **/
+A successful call will return ONLY the contents of the "result"
+element of the returned JSON object. Our calling code should not
+care if the RPC call was JSONRPC variety or some other type. Thus,
+none of the "packetizer" metadata is returned to the Success callback.
+
+If an error happens (either AJAX, or server error communicated as
+non-null Error element in the response) we call the error callback
+with the contents (actual of received, or simulated) of the Error
+element of JSONRPC response object. It has specific structure:
+
+Althught the plugin targets JSONRPC v1.0, because v1.0 does
+not define what "error" element must look like, we borrow 
+the error object structure from JSONRPC v2.0. Thus, when AJAX error
+callback is called, it gets the contents of "error" element per
+JSONRPC v2.0.
+
+
+Usage example:
+
+$.JSONRPC.call(
+    "http://example.com/rpc", 
+    method, 
+    params,
+    function (answer) {
+            // answer = JS object - the actual return value of the RPC method.
+    },
+    function (answer) {
+            // answer = error object per JSONRPC v2.0 spec. 
+            // Ex: {'code':#, 'message':'...', 'data': some_object_but_likely_string}
+    }
+})
+
+This file is part of Git Enablement Server Project.
+
+Git Enablement Server Project is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Git Enablement Server Project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Git Enablement Server Project.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 (function($) {
-	$.extend({
-		JsonRPC: {
-			endPoint: null,
-			request: function (url, method, params, callbacks) {
-			    this.version = '2.0';
-			    this.url = url;
-			    this.method = method;
-			    this.params = params;
-			    this.id = 1;
-
-			    var postData = {};
-
-			    // postData.jsonrpc = this.version;
-			    postData.method = this.method;
-			    postData.params = this.params;
-
-			    var ajaxCall = {};
-			    ajaxCall.type = 'POST';
-			    ajaxCall.url = this.url + '?tm=' + new Date().getTime();
-			    ajaxCall.data = JSON.stringify(postData);
-			    ajaxCall.cache = false;
-			    ajaxCall.processData = false;
-
-			    ajaxCall.error = function (json) {
-			        if (callbacks.error) {
-			            callbacks.error (new $.JsonRPC.response());
-			        } 
-			    }
-			    ajaxCall.success = function (json) {
-			        var response = new $.JsonRPC.response (json);
-//			        if (response.error) {
-//			            if (callbacks.error) {
-//			                callbacks.error (response);
-//			            }
-//			        }
-			        
-			        if (callbacks.success) {
-			            callbacks.success (response);
-			        } 
-			    }
-			    
-			    $.ajax(ajaxCall);
-			},
-			
-			response: function (json) {
-			    if (!json) {
-			        this.error = 'Internal server error';
-			    }
-			    else {
-			        try {
-			            var obj = eval ( '(' + json + ')' );
-			            this.id = obj.id;
-			            this.result = obj.result;
-			            this.error = obj.error;
-			        }
-			        catch (e) {
-			            this.error = 'Internal server error: ' + e;
-			            this.version = '2.0';
-			        }
-			    }
-			}
-		}
-	});
+    var _JSONRPC_request_ID_prefix = Math.floor(Math.random() * 10000).toString(16)
+    $.extend({
+        JSONRPC: {
+            call: function (uri, method, params, success_call, error_call) {
+                var _id = _JSONRPC_request_ID_prefix + (new Date()).getTime().toString(16)
+                var ajaxSettings = {
+                    type: 'POST'
+                    ,url: uri
+                    ,data: JSON.stringify({'id':_id,'method':method,'params':params})
+                    ,cache: false
+                    ,processData: false
+                    ,error: function (obj) {
+                        if (error_call) {
+                            error_call(obj)
+                        }
+                    }
+                    ,success: function (obj) {
+                        try {
+                            var jsobj = obj , _e, _r
+                            if (_id != jsobj.id) {
+                                throw "Incorrect ID element in the response JSON"
+                            }
+                            if (jsobj.error) {
+                                throw jsobj.error
+                            }
+                            //if (!jsobj.result) {
+                            //    throw "Returned Result element is empty"
+                            success_call(jsobj.result);
+                        }
+                        catch (obj) {
+                            if (error_call) {
+                                error_call(obj)
+                            }
+                        }
+                    }
+                }
+                $.ajax(ajaxSettings);
+            }
+        }
+    })
 })(jQuery);
