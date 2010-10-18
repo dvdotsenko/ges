8ac7720ac2bd19ab4ef2c392f45456750e2f41aa
D. Dotsenko <dotsa@hotmail.com>
2010-10-17 18:54:02 -0700

    Testing switches to prefabbed, zipped folders tree with prefabbed git repos.

--- test_ges_rpc_methods.py
+++ test_ges_rpc_methods.py
@@ -24,24 +24,18 @@ import sys
 execpath = os.getcwd()
 sys.path.append(os.path.join(execpath, 'gitpython\lib'))
 
-import io
 import unittest
 import ges_rpc_methods as grm
 import tempfile
 import shutil
-import subprocess
+import zipfile
 
 class test_GesRPCMethods(unittest.TestCase):
 
     def setUp(self):
-        self.base_path = tempfile.mkdtemp()
-        dirone = os.path.abspath(os.path.join(self.base_path,'repos/repoone'))
-        os.makedirs(dirone)
-        if subprocess.call('git init --quiet --bare "%s"' % dirone):
-            raise Exception('Initing sample git repo failed.')
-        os.makedirs(
-            os.path.abspath(os.path.join(self.base_path,'repos/folderone/'))
-            )
+        _p = tempfile.mkdtemp()
+        zipfile.ZipFile('./test/sample_tree_of_repos_v1.zip').extractall(_p)
+        self.base_path = os.path.join(_p, 'reposbase')
         self._rpc_tree = dict(grm.assemble_methods_list(self.base_path))
 
     def tearDown(self):
@@ -49,6 +43,44 @@ class test_GesRPCMethods(unittest.TestCase):
 
     def test_01_browser_methods(self):
         _m = self._rpc_tree['browser.listdir']
+        self.assertEquals(
+            _m(''),
+            {'path':'/', 'dirs':[{'name':'projects'},{'name':'teams'},{'name':'users'}]}
+        )
+        self.assertEquals(
+            _m('/'),
+            {'path':'/', 'dirs':[{'name':'projects'},{'name':'teams'},{'name':'users'}]}
+        )
+        self.assertEquals(
+            _m('\\'),
+            {'path':'/', 'dirs':[{'name':'projects'},{'name':'teams'},{'name':'users'}]}
+        )
+        # crossing fingers and hoping the order is same on all platforms.
+        self.assertEquals(
+            _m('projects'),
+            {'path':'/projects', 'dirs':[
+                {'name':'common_files'},
+                {'name':'demorepoone','is_git_dir':True},
+                {'name':'projectone','is_git_dir':True}
+            ]}
+        )
+        self.assertEquals(
+            _m('projects/common_files'),
+            {'path':'/projects/common_files', 'dirs':[]}
+        )
+        # we don't allow seeing files / folders inside repo folders
+        self.assertRaises(grm.PathUnfitError, _m, 'projects/demorepoone')
+        self.assertRaises(grm.PathUnfitError, _m, 'projects/demorepoone/objects')
+        # on top of fobiden, it also does not exist.
+        self.assertRaises(grm.PathUnfitError, _m, 'projects/demorepoone/kjhgjg')
+        # all these should not exist
+        self.assertRaises(grm.PathUnfitError, _m, 'projects/blah')
+        self.assertRaises(grm.PathUnfitError, _m, '/blah')
+        # we should forbid seeing contents of folders above base path.
+        self.assertRaises(grm.PathUnfitError, _m, 'projects/../../../blah')
+
+    def dtest_02_repoview_methods(self):
+        _m = self._rpc_tree['repoview.endpoints']
         self.assertEquals(
             _m(''),
             {'path':'', 'dirs':[{'name':'repos'}]}
@@ -76,7 +108,7 @@ class test_GesRPCMethods(unittest.TestCase):
         self.assertRaises(grm.PathUnfitError, _m, 'repos/blah')
         self.assertRaises(grm.PathUnfitError, _m, 'repos/../../../blah')
         self.assertRaises(grm.PathUnfitError, _m, '/blah')
- 
+
 if __name__ == "__main__":
     unittest.TextTestRunner(verbosity=2).run(
         unittest.TestSuite([
